19c94fb4e90d288f1499a35d3a8dceb87e72bddb
tseijp <_@tsei.jp>
2026-02-19 19:57:02 +0900

    feat: add socket app (#20)

--- projects/app/src/client.tsx
+++ projects/app/src/client.tsx
@@ -1,4 +1,36 @@
-// src/client.tsx
+// client.tsx
 import './style.css'
+import { signIn } from '@hono/auth-js/react'
+import { usePartySocket } from 'partysocket/react'
+import { useState } from 'react'
 import { createRoot } from 'react-dom/client'
-createRoot(document.getElementById('root')!).render('ok')
+import useSWR from 'swr'
+
+const Cursors = () => {
+        const [users, set] = useState([] as [username: string, transform: string][])
+        const socket = usePartySocket({
+                party: 'v1',
+                room: 'my-room',
+                onOpen: () => window.addEventListener('mousemove', (e) => socket.send(`translate(${e.clientX}px, ${e.clientY}px)`)),
+                onMessage: (e) => set(Object.entries(JSON.parse(e.data))),
+        })
+        return users.map(([username, transform]) => (
+                <div key={username} className="absolute" style={{ transform }}>
+                        {username}
+                </div>
+        ))
+}
+
+const fetcher = async () => {
+        const res = await fetch('/api/v1/me')
+        return await res.json()
+}
+
+const App = () => {
+        const { data, isLoading } = useSWR('me', fetcher)
+        if (isLoading) return 'Loading...'
+        if (!data) return <button onClick={() => signIn()}>Sign In</button>
+        return <Cursors />
+}
+
+createRoot(document.getElementById('root')!).render(<App />)
--- projects/app/src/index.tsx
+++ projects/app/src/index.tsx
@@ -1,4 +1,64 @@
-// src/index.tsx
+// index.tsx
+import Google from '@auth/core/providers/google'
+import { DrizzleAdapter } from '@auth/drizzle-adapter'
+import { authHandler, initAuthConfig, verifyAuth } from '@hono/auth-js'
+import { eq } from 'drizzle-orm'
+import { drizzle } from 'drizzle-orm/d1'
 import { Hono } from 'hono'
-export default new Hono().get('/api/res', (c) => c.text('ok'))
-// src/renderer.ts REMOVE
+import { env } from 'hono/adapter'
+import { createMiddleware } from 'hono/factory'
+import { routePartykitRequest, Server } from 'partyserver'
+import { users } from './schema'
+import type { Connection, ConnectionContext } from 'partyserver'
+
+const authMiddleware = initAuthConfig((c) => ({
+        adapter: DrizzleAdapter(drizzle(c.env.my_d1_tmp)),
+        providers: [Google({ clientId: c.env.GOOGLE_CLIENT_ID, clientSecret: c.env.GOOGLE_CLIENT_SECRET })],
+        secret: c.env.AUTH_SECRET,
+        session: { strategy: 'jwt' },
+}))
+
+const myMiddleware = createMiddleware(async (c) => {
+        const headers = new Headers(c.req.raw.headers)
+        headers.set('x-auth-sub', c.get('authUser')?.token?.sub!)
+        const req = new Request(c.req.raw, { headers })
+        const res = await routePartykitRequest(req, env(c))
+        return res ?? c.text('Not Found', 404)
+})
+
+const getUserBySub = (DB: D1Database, sub: string) => drizzle(DB).select().from(users).where(eq(users.id, sub)).limit(1)
+
+type Env = { my_d1_tmp: D1Database; my_r2_tmp: R2Bucket }
+type Conn = Connection<{ username: string }>
+
+export class PartyServer extends Server<Env> {
+        users = {} as Record<string, string>
+        static options = { hibernate: true }
+        async onConnect(conn: Conn, c: ConnectionContext) {
+                const sub = c.request.headers.get('x-auth-sub')!
+                const [user] = await getUserBySub(this.env.my_d1_tmp, sub)
+                conn.setState({ username: user.name! })
+        }
+        async onMessage(conn: Conn, message: string) {
+                this.users[conn.state!.username] = message
+                this.broadcast(JSON.stringify(this.users))
+        }
+        onClose(conn: Conn) {
+                delete this.users[conn.state!.username]
+                this.broadcast(JSON.stringify(this.users), [conn.id])
+        }
+}
+
+export default new Hono<{ Bindings: Env }>()
+        .get('/api/res', (c) => c.text('ok'))
+        .use('*', authMiddleware)
+        .use('/parties/*', verifyAuth())
+        .use('/parties/*', myMiddleware)
+        .use('/api/auth/*', authHandler())
+        .use('/api/v1/*', verifyAuth())
+        .get('/api/v1/me', async (c) => {
+                const { token } = c.get('authUser')
+                if (!token || !token.sub) return c.json(null, 401)
+                const [user] = await getUserBySub(c.env.my_d1_tmp, token.sub)
+                return c.json({ username: user.name || null })
+        })
