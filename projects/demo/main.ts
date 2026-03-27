02e2dc9ee35b29f90606ead9b747b41b7be5049f
tseijp <_@tsei.jp>
2026-03-27 18:03:32 +0900

    feat: migrate to webgpu (#22)

--- projects/demo/main.ts
+++ projects/demo/main.ts
@@ -1,10 +1,14 @@
-import { createGL } from 'glre/src'
-import { box } from 'glre/src/buffers'
-import { float, Fn, If, instance, int, ivec2, ivec3, mat4, Scope, texelFetch, texture2D, uniform, varying, vec3, vec4 } from 'glre/src/node'
+// import { createGL } from 'glre/src'
+// import { box } from 'glre/src/buffers'
+// import { float, Fn, If, instance, int, ivec2, ivec3, mat4, Scope, texelFetch, texture2D, uniform, varying, vec3, vec4 } from 'glre/src/node'
+import { createGL } from '../../../../packages/core/src'
+import { box } from '../../../../packages/core/src/buffers'
+import { float, Fn, If, instance, mat4, Scope, texelFetch, texture2D, uint, uniform, uvec2, uvec3, varying, vec3, vec4 } from '../../../../packages/core/src/node'
 import { createCamera, createMesh, createScene, range } from 'voxelized-js/src'
 import VoxelWorker from './worker?worker'
 
-import type { Float, Int, IVec2, IVec3, Vec3 } from 'glre/src/node'
+// import type { Float, Int, IVec2, IVec3, Vec3 } from 'glre/src/node'
+import type { Float, UInt, UVec2, UVec3, Vec3 } from '../../../../packages/core/src/node'
 
 const iMVP = uniform<'mat4'>(mat4(), 'iMVP')
 const cube = box()
@@ -16,36 +20,37 @@ const scl = instance<'vec3'>(vec3(), 'scl')
 const pos = instance<'vec3'>(vec3(), 'pos')
 const aid = instance<'float'>(float(), 'aid')
 const vCenter = varying<'vec3'>(vec3(), 'vCenter')
-const xyz2m = Fn(([xyz]: [IVec3]): Int => {
+const xyz2m = Fn(([xyz]: [UVec3]): UInt => {
         const p = xyz.toVar()
-        p.bitOrAssign(p.shiftLeft(int(16)))
-        p.bitAndAssign(ivec3(int(0xff0000ff)))
-        p.bitOrAssign(p.shiftLeft(int(8)))
-        p.bitAndAssign(ivec3(int(0x0300f00f)))
-        p.bitOrAssign(p.shiftLeft(int(4)))
-        p.bitAndAssign(ivec3(int(0x030c30c3)))
-        p.bitOrAssign(p.shiftLeft(int(2)))
-        p.bitAndAssign(ivec3(int(0x09249249)))
-        return p.x.bitOr(p.y.shiftLeft(int(1))).bitOr(p.z.shiftLeft(int(2)))
+        p.bitOrAssign(p.shiftLeft(uvec3(uint(16))))
+        p.bitAndAssign(uvec3(uint(0xff0000ff)))
+        p.bitOrAssign(p.shiftLeft(uvec3(uint(8))))
+        p.bitAndAssign(uvec3(uint(0x0300f00f)))
+        p.bitOrAssign(p.shiftLeft(uvec3(uint(4))))
+        p.bitAndAssign(uvec3(uint(0x030c30c3)))
+        p.bitOrAssign(p.shiftLeft(uvec3(uint(2))))
+        p.bitAndAssign(uvec3(uint(0x09249249)))
+        return p.x.bitOr(p.y.shiftLeft(uint(1))).bitOr(p.z.shiftLeft(uint(2)))
 })
-const m2uv = Fn(([morton]: [Int]): IVec2 => {
-        const p = ivec2(morton, morton.shiftRight(int(1))).toVar()
-        p.bitAndAssign(ivec2(int(0x55555555)))
-        p.bitOrAssign(p.shiftRight(int(1)))
-        p.bitAndAssign(ivec2(int(0x33333333)))
-        p.bitOrAssign(p.shiftRight(int(2)))
-        p.bitAndAssign(ivec2(int(0x0f0f0f0f)))
-        p.bitOrAssign(p.shiftRight(int(4)))
-        p.bitAndAssign(ivec2(int(0x00ff00ff)))
-        p.bitOrAssign(p.shiftRight(int(8)))
-        p.bitAndAssign(ivec2(int(0x0000ffff)))
+const m2uv = Fn(([morton]: [UInt]): UVec2 => {
+        const p = uvec2(morton, morton.shiftRight(uint(1))).toVar()
+        p.bitAndAssign(uvec2(uint(0x55555555)))
+        p.bitOrAssign(p.shiftRight(uvec2(uint(1))))
+        p.bitAndAssign(uvec2(uint(0x33333333)))
+        p.bitOrAssign(p.shiftRight(uvec2(uint(2))))
+        p.bitAndAssign(uvec2(uint(0x0f0f0f0f)))
+        p.bitOrAssign(p.shiftRight(uvec2(uint(4))))
+        p.bitAndAssign(uvec2(uint(0x00ff00ff)))
+        p.bitOrAssign(p.shiftRight(uvec2(uint(8))))
+        p.bitAndAssign(uvec2(uint(0x0000ffff)))
         return p
 })
-const pick = Fn(([id, uvPix]: [Float, IVec2]) => {
+const pick = Fn(([id, uvPix]: [Float, UVec2]) => {
+        const uv = uvPix.toIVec2().toVar()
         const t = vec4(0, 0, 0, 1).toVar('t')
         range(16).map((i) => {
                 If(id.equal(i), () => {
-                        t.assign(texelFetch(iAtlas[i], uvPix, int(0)))
+                        t.assign(texelFetch(iAtlas[i], uv, uint(0)))
                 })
         })
         return t
@@ -67,7 +72,7 @@ const vert = Scope(() => {
         return iMVP.mul(vec4(world, 1))
 })
 const frag = Scope(() => {
-        const p = vCenter.toIVec3()
+        const p = vCenter.toUVec3()
         const d = varying(diffuse(normal))
         const i = varying(aid)
         const uv = m2uv(xyz2m(p)).toVar('uv')
@@ -86,10 +91,12 @@ let lastVersion = -1
 const gl = createGL({
         precision: 'highp',
         isWebGL: false,
+        // isWebGL: true,
         isDepth: true,
         // wireframe: true,
         triangleCount: 12,
         instanceCount: 1,
+        isDebug: true,
         vert,
         frag,
         render() {
