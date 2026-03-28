5076577267fd11b4201cb03dfc8eb2672634f0bd
tseijp <_@tsei.jp>
2026-03-28 18:15:18 +0900

    feat: support uniform array (#22)

--- projects/demo/main.ts
+++ projects/demo/main.ts
@@ -1,13 +1,8 @@
-// import { createGL } from 'glre/src'
-// import { box } from 'glre/src/buffers'
-// import { float, Fn, If, instance, int, ivec2, ivec3, mat4, Scope, texelFetch, texture2D, uniform, varying, vec3, vec4 } from 'glre/src/node'
 import { createGL } from '../../../../packages/core/src'
 import { box } from '../../../../packages/core/src/buffers'
-import { float, Fn, If, instance, mat4, Scope, texelFetch, texture2D, uint, uniform, uvec2, uvec3, varying, vec3, vec4 } from '../../../../packages/core/src/node'
+import { float, Fn, If, instance, mat4, Scope, texelFetch, texture2D, uint, uniform, uniformArray, uvec2, uvec3, varying, vec3, vec4 } from '../../../../packages/core/src/node'
 import { createCamera, createMesh, createScene, range } from 'voxelized-js/src'
 import VoxelWorker from './worker?worker'
-
-// import type { Float, Int, IVec2, IVec3, Vec3 } from 'glre/src/node'
 import type { Float, UInt, UVec2, UVec3, Vec3 } from '../../../../packages/core/src/node'
 
 const iMVP = uniform<'mat4'>(mat4(), 'iMVP')
@@ -15,11 +10,13 @@ const cube = box()
 const vertex = cube.vertex('vertex')
 const normal = cube.normal('normal')
 const iAtlas = range(16).map((i) => uniform(texture2D(), `iAtlas${i}`))
-const iOffset = range(16).map((i) => uniform(vec3(0, 0, 0), `iOffset${i}`))
+const iOffset = uniformArray(vec4(), 'iOffset', 16)
 const scl = instance<'vec3'>(vec3(), 'scl')
 const pos = instance<'vec3'>(vec3(), 'pos')
 const aid = instance<'float'>(float(), 'aid')
 const vCenter = varying<'vec3'>(vec3(), 'vCenter')
+const vDiffuse = varying<'float'>(float(), 'vDiffuse')
+const vAid = varying<'float'>(float(), 'vAid')
 const xyz2m = Fn(([xyz]: [UVec3]): UInt => {
         const p = xyz.toVar()
         p.bitOrAssign(p.shiftLeft(uvec3(uint(16))))
@@ -59,24 +56,19 @@ const diffuse = Fn(([n]: [Vec3]) => {
         return vec3(-0.33, 0.77, 0.55).normalize().dot(n).mul(0.5).add(0.5)
 })
 const vert = Scope(() => {
-        const off = vec3(0, 0, 0).toVar('off')
-        range(16).forEach((i) => {
-                If(aid.equal(i), () => {
-                        off.assign(iOffset[i])
-                })
-        })
+        const off = iOffset.element(aid.toInt()).xyz
         const local = vertex.mul(scl).add(pos)
         const world = off.add(local)
         const center = local.sub(normal.sign().mul(0.5)).floor()
         vCenter.assign(center)
+        vDiffuse.assign(diffuse(normal))
+        vAid.assign(aid)
         return iMVP.mul(vec4(world, 1))
 })
 const frag = Scope(() => {
         const p = vCenter.toUVec3()
-        const d = varying(diffuse(normal))
-        const i = varying(aid)
         const uv = m2uv(xyz2m(p)).toVar('uv')
-        const rgb = pick(i, uv).rgb.mul(d).toVar('rgb')
+        const rgb = pick(vAid, uv).rgb.mul(vDiffuse).toVar('rgb')
         return vec4(rgb, 1)
 })
 const worker = new VoxelWorker()
@@ -87,13 +79,13 @@ const scene = createScene(mesh, cam, worker)
 let ts = performance.now()
 let pt = ts
 let lastVersion = -1
+const iOffsetData = new Float32Array(16 * 4)
 
 const gl = createGL({
         precision: 'highp',
-        isWebGL: false,
-        // isWebGL: true,
+        // isWebGL: false,
+        isWebGL: true,
         isDepth: true,
-        // wireframe: true,
         triangleCount: 12,
         instanceCount: 1,
         isDebug: true,
@@ -108,9 +100,12 @@ const gl = createGL({
                 gl._uniform?.('iMVP', [...cam.MVP])
                 scene.render()
                 scene.slots.getUpdates().forEach(({ index, bitmap, offset }) => {
-                        gl._uniform?.(`iOffset${index}`, offset)
+                        iOffsetData[index * 4] = offset[0]
+                        iOffsetData[index * 4 + 1] = offset[1]
+                        iOffsetData[index * 4 + 2] = offset[2]
                         gl._texture?.(`iAtlas${index}`, bitmap)
                 })
+                gl._uniform?.('iOffset', iOffsetData)
                 const data = mesh.getData()
                 if (data.version === lastVersion) return
                 lastVersion = data.version
